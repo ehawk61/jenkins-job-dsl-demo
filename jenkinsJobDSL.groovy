905eae2e45ca7802e4ac77059ec670ba65308553
Jonathan L. Meek <jonathan.l.meek@gmail.com>
2018-12-28 06:48:50 -0500

    Added jenkinsJobDSL.groovy as part of a demo

---
+++ jenkinsJobDSL.groovy
@@ -0,0 +1,24 @@
+/*Jenkins Job DSL Demo Script
+
+ */ 
+def interfaces = ['foo-interfaceA', 'foo-interfaceB', 'foo-interfaceC', 'foo-interfaceD', 'foo-interfaceE', 'foo-interfaceF']
+def release = 'R1812' 
+
+interfaces.each{
+  freestyleJob("$release-$it"){
+    jdk('1.7')
+    scm{
+      svn("svn://svn.mydomain.com/project1/$release/$it")
+    }
+    steps{
+      maven('clean install')
+    }
+  }
+}
+listView("$release-foo-interfaces"){
+  description("All jobs for $release-foo-interfaces")
+  jobs {
+    interfaces.each{ name("$release-$it") }
+  }
+}
+
