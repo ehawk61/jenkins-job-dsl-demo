068e6bc9cff8991ddad81569e9b06845f5c695fc
Jonathan L. Meek <jonathan.l.meek@gmail.com>
2018-12-29 14:52:22 -0500

    Added jenkinsJobDSLRevisited.groovy and made initial change

---
+++ jenkinsJobDSLRevisited.groovy
@@ -0,0 +1,34 @@
+/* Jenkins Job DSL Demo Script Revisited*/
+ 
+def interfaces = ['foo-interfaceA', 'foo-interfaceB', 'foo-interfaceC', 'foo-interfaceD', 'foo-interfaceE', 'foo-interfaceF']
+
+def release = $RELEASE 
+
+interfaces.each{
+  def interfaceName = "$release-$it"; 
+  freeStyleJob(interfaceName){
+    jdk('1.7')
+    scm{
+      svn("svn://svn.mydomain.com/project1/$release/" + interfaceName.replaceAll("$release-",""))
+    }
+    steps{
+      maven('clean install')
+    }
+  }
+}
+listView("$release-foo-interfaces"){
+  description("All jobs for $release-foo-interfaces")
+  jobs {
+    interfaces.each{ name("$release-$it") }
+  }
+  columns {
+    status()
+    weather()
+    name()
+    lastSuccess()
+    lastFailure()
+    lastDuration()
+    buildButton()
+  }
+}
+
